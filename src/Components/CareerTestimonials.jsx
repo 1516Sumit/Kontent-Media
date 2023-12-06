b3bfc6240de0ffa0ba5733893382e0ec549a4f64
1516Sumit <sumitsharma781425@gmail.com>
2023-12-06 19:26:42 +0530

    add iphone background

--- src/Components/CareerTestimonials.jsx
+++ src/Components/CareerTestimonials.jsx
@@ -5,22 +5,27 @@ import Slider from "./Slider"
 function CareerTestimonials() {
   return (
     <>
-    <div className="bg-black flex items-center justify-between flex-col md:pt-20">
-        <div className="mt-5 mb-7 md:mb-7 text-center">
-            <p className="self-center text-2xl sm:text-4xl md:text-5xl tracking-tight font-semibold whitespace-nowrap text-white" style={{ fontFamily: 'Archivo' }}>
-                You are not
-                <span className="self-center text-2xl sm:text-4xl md:text-5xl tracking-tight font-semibold whitespace-nowrap text-[#7F00FF]" style={{ fontFamily: 'Archivo' }}> alone.</span>
-                The pain
-                <br />
-                of website
-                <span className="self-center text-2xl sm:text-4xl md:text-5xl tracking-tight font-semibold whitespace-nowrap text-[#FFD93D]" style={{ fontFamily: 'Archivo' }}> creation </span>
-                is real
-            </p>
+      <div>
+        <div className="bg-black h-0 block relative z-0 left-0" style={{ top: '60px' }}>
+          <img className="w-32 sm:w-36 md:w-40" src="./images/Mask group.svg" />
         </div>
+        <div className="bg-black flex items-center justify-between flex-col md:pt-20">
+          <div className="mt-5 mb-7 md:mb-7 text-center">
+            <p className="self-center text-2xl sm:text-4xl md:text-5xl tracking-tight font-semibold whitespace-nowrap text-white" style={{ fontFamily: 'Archivo' }}>
+              You are not
+              <span className="self-center text-2xl sm:text-4xl md:text-5xl tracking-tight font-semibold whitespace-nowrap text-[#7F00FF]" style={{ fontFamily: 'Archivo' }}> alone.</span>
+              
+              <br />
+              Success
+              <span className="self-center text-2xl sm:text-4xl md:text-5xl tracking-tight font-semibold whitespace-nowrap text-[#FFD93D]" style={{ fontFamily: 'Archivo' }}> Edit Story </span>
+             
+            </p>
+          </div>
 
-        <Slider />
-    </div> 
-</>
+          <Slider />
+        </div>
+      </div>
+    </>
   )
 }
 
